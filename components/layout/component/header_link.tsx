1da785c0ac8c79ef815061008edd0073a9369bbf
Nicolas Durand <nicolas.durand@sotiaf.fr>
2024-06-10 22:15:23 +0200

    :sparkles: Menu

--- components/layout/component/header_link.tsx
+++ components/layout/component/header_link.tsx
@@ -7,17 +7,17 @@ import { tinaField } from "tinacms/dist/react";
 const customTheme: CustomFlowbiteTheme = {
   dropdown: {
     arrowIcon: "ml-2 h-4 w-4",
-    content: "py-1 focus:outline-none",
+    content: "py-1 px-3 space-y-2 text-white bg-lunar-green focus:outline-none",
     floating: {
       animation: "transition-opacity",
-      base: "z-10 w-fit divide-y divide-gray-100 rounded shadow focus:outline-none",
-      content: "py-1 text-sm text-gray-700 dark:text-gray-200",
+      base: "z-10 w-fit divide-y divide-gray-100 rounded shadow focus:outline-none !border-lunar-green",
+      content: "py-1 text-sm text-gray-700 border-lunar-green",
       divider: "my-1 h-px bg-gray-100 dark:bg-gray-600",
       header: "block px-4 py-2 text-sm text-gray-700 dark:text-gray-200",
       hidden: "invisible opacity-0",
       item: {
         container: "",
-        base: "flex w-full cursor-pointer items-center justify-start px-4 py-2 text-sm text-white bg-luner-green hover:text-secondary focus:bg-secondary focus:outline-none",
+        base: "flex w-full cursor-pointer items-center justify-start px-4 py-2 text-sm text-white bg-lunar-green hover:bg-lunar-green hover:text-secondary focus:outline-none",
         icon: "mr-2 h-4 w-4",
       },
       style: {
@@ -32,39 +32,48 @@ const customTheme: CustomFlowbiteTheme = {
 
 export const HeaderLink = ({ item }) => {
   const router = useRouter();
-  const isPathRelative = (path) => {
-    return path.indexOf("http://") === 0 || path.indexOf("https://") === 0;
-  };
 
   const [isClient, setIsClient] = React.useState(false);
   React.useEffect(() => {
     setIsClient(true);
   }, []);
 
-  const activeItem =
-    (item.href === ""
-      ? router.asPath === "/"
-      : router.asPath.includes(item.href)) && isClient;
+  const isUrlRelative = (url: string) => {
+    let isRelative = true;
+    if (url.indexOf("http://") === 0 || url.indexOf("https://") === 0)
+      isRelative = false;
+    return isRelative;
+  };
+
+  const isUrlActive = (url: string) => {
+    return (
+      (url === "" ? router.asPath === "/" : router.asPath.includes(url)) &&
+      isClient
+    );
+  };
 
   return (
     <>
       {item.nav ? (
         <Flowbite theme={{ theme: customTheme, mode: "light" }}>
           <Dropdown label={item.label} color={"primary"} inline>
-            <Dropdown.Item>Dashboard</Dropdown.Item>
-            <Dropdown.Item>Settings</Dropdown.Item>
-            <Dropdown.Item>Earnings</Dropdown.Item>
-            <Dropdown.Item>Sign out</Dropdown.Item>
+            {item.nav.map((subItem, i) => (
+              <Dropdown.Item
+                key={i}
+                as={HeaderLink}
+                item={subItem}
+              ></Dropdown.Item>
+            ))}
           </Dropdown>
         </Flowbite>
       ) : (
         <>
-          {isPathRelative(item.href) ? (
+          {isUrlRelative(item.href) ? (
             <Link
               data-tina-field={tinaField(item, "href")}
               href={`/${item.href}`}
               className={`block py-2 text-center transition ease-in-out hover:text-secondary md:p-0 ${
-                activeItem && "text-secondary"
+                isUrlActive(item.href) ? "text-secondary" : "text-white"
               }`}
               aria-current="page"
             >
